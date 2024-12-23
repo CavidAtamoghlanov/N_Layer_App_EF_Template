5c2a47e90a05ba96e2625b8e3cd89d9f9bbc0e1a
Cavid Atamoghlanov <91869829+CavidAtamoghlanov@users.noreply.github.com>
2024-12-23 18:43:48 +0400

    Add Static keyword

--- N_Layer_App_EF_Template.Business/Responses/ResponceMethods.cs
+++ N_Layer_App_EF_Template.Business/Responses/ResponceMethods.cs
@@ -3,79 +3,79 @@ using N_Layer_App_EF_Template.Domain.ResponseModels;
 
 namespace N_Layer_App_EF_Template.Business.Responses
 {
-    public class ResponseMethods
+    public abstract class ResponseMethods
     {
-        public Response Success(object? content = null, string? exceptionMessage = null, string? innerExceptionMessage = null)
+        public static Response Success(object? content = null, string? exceptionMessage = null, string? innerExceptionMessage = null)
         {
             return new Response(HttpStatusCode.Success, content: content, exceptionMessage: exceptionMessage, innerExceptionMessage: innerExceptionMessage);
         }
 
-        public Response Created(object? content = null)
+        public static Response Created(object? content = null)
         {
             return new Response(HttpStatusCode.Created, content: content);
         }
 
-        public Response NoContent()
+        public static Response NoContent()
         {
             return new Response(HttpStatusCode.NoContent);
         }
 
-        public Response BadRequest(string exceptionMessage, string? innerExceptionMessage = null, object? content = null)
+        public static Response BadRequest(string exceptionMessage, string? innerExceptionMessage = null, object? content = null)
         {
             return new Response(HttpStatusCode.BadRequest, exceptionMessage: exceptionMessage, innerExceptionMessage: innerExceptionMessage, content: content);
         }
 
-        public Response Unauthorized(string exceptionMessage, string? innerExceptionMessage = null, object? content = null)
+        public static Response Unauthorized(string exceptionMessage, string? innerExceptionMessage = null, object? content = null)
         {
             return new Response(HttpStatusCode.Unauthorized, exceptionMessage: exceptionMessage, innerExceptionMessage: innerExceptionMessage, content: content);
         }
 
-        public Response Forbidden(string exceptionMessage, string? innerExceptionMessage = null, object? content = null)
+        public static Response Forbidden(string exceptionMessage, string? innerExceptionMessage = null, object? content = null)
         {
             return new Response(HttpStatusCode.Forbidden, exceptionMessage: exceptionMessage, innerExceptionMessage: innerExceptionMessage, content: content);
         }
 
-        public Response NotFound(string exceptionMessage, string? innerExceptionMessage = null, object? content = null)
+        public static Response NotFound(string exceptionMessage, string? innerExceptionMessage = null, object? content = null)
         {
             return new Response(HttpStatusCode.NotFound, exceptionMessage: exceptionMessage, innerExceptionMessage: innerExceptionMessage, content: content);
         }
 
-        public Response MethodNotAllowed(string exceptionMessage, string? innerExceptionMessage = null, object? content = null)
+        public static Response MethodNotAllowed(string exceptionMessage, string? innerExceptionMessage = null, object? content = null)
         {
             return new Response(HttpStatusCode.MethodNotAllowed, exceptionMessage: exceptionMessage, innerExceptionMessage: innerExceptionMessage, content: content);
         }
 
-        public Response Conflict(string exceptionMessage, string? innerExceptionMessage = null, object? content = null)
+        public static Response Conflict(string exceptionMessage, string? innerExceptionMessage = null, object? content = null)
         {
             return new Response(HttpStatusCode.Conflict, exceptionMessage: exceptionMessage, innerExceptionMessage: innerExceptionMessage, content: content);
         }
 
-        public Response UnprocessableEntity(string exceptionMessage, string? innerExceptionMessage = null, object? content = null)
+        public static Response UnprocessableEntity(string exceptionMessage, string? innerExceptionMessage = null, object? content = null)
         {
             return new Response(HttpStatusCode.UnprocessableEntity, exceptionMessage: exceptionMessage, innerExceptionMessage: innerExceptionMessage, content: content);
         }
 
-        public Response Error(string exceptionMessage, string? innerExceptionMessage = null, object? content = null)
+        public static Response Error(string exceptionMessage, string? innerExceptionMessage = null, object? content = null)
         {
             return new Response(HttpStatusCode.InternalServerError, exceptionMessage: exceptionMessage, innerExceptionMessage: innerExceptionMessage, content: content);
         }
 
-        public Response NotImplemented(string message, string? innerExceptionMessage = null, object? content = null)
+        public static Response NotImplemented(string message, string? innerExceptionMessage = null, object? content = null)
         {
             return new Response(HttpStatusCode.NotImplemented, exceptionMessage: message, innerExceptionMessage: innerExceptionMessage, content: content);
         }
 
-        public Response ServiceUnavailable(string message, string? innerExceptionMessage = null, object? content = null)
+        public static Response ServiceUnavailable(string message, string? innerExceptionMessage = null, object? content = null)
         {
             return new Response(HttpStatusCode.ServiceUnavailable, exceptionMessage: message, innerExceptionMessage: innerExceptionMessage, content: content);
         }
 
-        public Response BadGateway(string message, string? innerExceptionMessage = null, object? content = null)
+        public static Response BadGateway(string message, string? innerExceptionMessage = null, object? content = null)
         {
             return new Response(HttpStatusCode.BadGateway, exceptionMessage: message, innerExceptionMessage: innerExceptionMessage, content: content);
         }
 
-        public Response GatewayTimeout(string message, string? innerExceptionMessage = null, object? content = null)
+        public static Response GatewayTimeout(string message, string? innerExceptionMessage = null, object? content = null)
         {
             return new Response(HttpStatusCode.GatewayTimeout, exceptionMessage: message, innerExceptionMessage: innerExceptionMessage, content: content);
         }
