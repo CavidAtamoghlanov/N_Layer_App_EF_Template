8a4142a6b5f162ac7f881e64cfd82b88db50e8e1
Cavid Atamoghlanov <91869829+CavidAtamoghlanov@users.noreply.github.com>
2024-12-23 18:29:46 +0400

    Change and Add new Method to ResponseMethod

--- N_Layer_App_EF_Template.Business/Responses/ResponceMethods.cs
+++ N_Layer_App_EF_Template.Business/Responses/ResponceMethods.cs
@@ -5,44 +5,14 @@ namespace N_Layer_App_EF_Template.Business.Responses
 {
     public class ResponseMethods
     {
-        public Response Success(object? content = null)
+        public Response Success(object? content = null, string? exceptionMessage = null, string? innerExceptionMessage = null)
         {
-            return new Response(HttpStatusCode.Success, content: content);
+            return new Response(HttpStatusCode.Success, content: content, exceptionMessage: exceptionMessage, innerExceptionMessage: innerExceptionMessage);
         }
 
-        public Response BadRequest(string exceptionMessage, string? innerExceptionMessage = null)
+        public Response Created(object? content = null)
         {
-            return new Response(HttpStatusCode.BadRequest, exceptionMessage: exceptionMessage, innerExceptionMessage: innerExceptionMessage);
-        }
-
-        public Response NotFound(string exceptionMessage, string? innerExceptionMessage = null)
-        {
-            return new Response(HttpStatusCode.NotFound, exceptionMessage: exceptionMessage, innerExceptionMessage: innerExceptionMessage);
-        }
-
-        public Response Unauthorized(string exceptionMessage, string? innerExceptionMessage = null)
-        {
-            return new Response(HttpStatusCode.Unauthorized, exceptionMessage: exceptionMessage, innerExceptionMessage: innerExceptionMessage);
-        }
-
-        public Response Forbidden(string exceptionMessage, string? innerExceptionMessage = null)
-        {
-            return new Response(HttpStatusCode.Forbidden, exceptionMessage: exceptionMessage, innerExceptionMessage: innerExceptionMessage);
-        }
-
-        public Response Error(string exceptionMessage, string? innerExceptionMessage = null)
-        {
-            return new Response(HttpStatusCode.InternalServerError, exceptionMessage: exceptionMessage, innerExceptionMessage: innerExceptionMessage);
-        }
-
-        public Response UnprocessableEntity(string exceptionMessage, string? innerExceptionMessage = null)
-        {
-            return new Response(HttpStatusCode.UnprocessableEntity, exceptionMessage: exceptionMessage, innerExceptionMessage: innerExceptionMessage);
-        }
-
-        public Response Conflict(string exceptionMessage, string? innerExceptionMessage = null)
-        {
-            return new Response(HttpStatusCode.Conflict, exceptionMessage: exceptionMessage, innerExceptionMessage: innerExceptionMessage);
+            return new Response(HttpStatusCode.Created, content: content);
         }
 
         public Response NoContent()
@@ -50,14 +20,64 @@ namespace N_Layer_App_EF_Template.Business.Responses
             return new Response(HttpStatusCode.NoContent);
         }
 
-        public Response MethodNotAllowed(string exceptionMessage, string? innerExceptionMessage = null)
+        public Response BadRequest(string exceptionMessage, string? innerExceptionMessage = null, object? content = null)
         {
-            return new Response(HttpStatusCode.MethodNotAllowed, exceptionMessage: exceptionMessage, innerExceptionMessage: innerExceptionMessage);
+            return new Response(HttpStatusCode.BadRequest, exceptionMessage: exceptionMessage, innerExceptionMessage: innerExceptionMessage, content: content);
         }
 
-        public Response RequestTimeout(string exceptionMessage, string? innerExceptionMessage = null)
+        public Response Unauthorized(string exceptionMessage, string? innerExceptionMessage = null, object? content = null)
         {
-            return new Response(HttpStatusCode.RequestTimeout, exceptionMessage: exceptionMessage, innerExceptionMessage: innerExceptionMessage);
+            return new Response(HttpStatusCode.Unauthorized, exceptionMessage: exceptionMessage, innerExceptionMessage: innerExceptionMessage, content: content);
+        }
+
+        public Response Forbidden(string exceptionMessage, string? innerExceptionMessage = null, object? content = null)
+        {
+            return new Response(HttpStatusCode.Forbidden, exceptionMessage: exceptionMessage, innerExceptionMessage: innerExceptionMessage, content: content);
+        }
+
+        public Response NotFound(string exceptionMessage, string? innerExceptionMessage = null, object? content = null)
+        {
+            return new Response(HttpStatusCode.NotFound, exceptionMessage: exceptionMessage, innerExceptionMessage: innerExceptionMessage, content: content);
+        }
+
+        public Response MethodNotAllowed(string exceptionMessage, string? innerExceptionMessage = null, object? content = null)
+        {
+            return new Response(HttpStatusCode.MethodNotAllowed, exceptionMessage: exceptionMessage, innerExceptionMessage: innerExceptionMessage, content: content);
+        }
+
+        public Response Conflict(string exceptionMessage, string? innerExceptionMessage = null, object? content = null)
+        {
+            return new Response(HttpStatusCode.Conflict, exceptionMessage: exceptionMessage, innerExceptionMessage: innerExceptionMessage, content: content);
+        }
+
+        public Response UnprocessableEntity(string exceptionMessage, string? innerExceptionMessage = null, object? content = null)
+        {
+            return new Response(HttpStatusCode.UnprocessableEntity, exceptionMessage: exceptionMessage, innerExceptionMessage: innerExceptionMessage, content: content);
+        }
+
+        public Response Error(string exceptionMessage, string? innerExceptionMessage = null, object? content = null)
+        {
+            return new Response(HttpStatusCode.InternalServerError, exceptionMessage: exceptionMessage, innerExceptionMessage: innerExceptionMessage, content: content);
+        }
+
+        public Response NotImplemented(string message, string? innerExceptionMessage = null, object? content = null)
+        {
+            return new Response(HttpStatusCode.NotImplemented, exceptionMessage: message, innerExceptionMessage: innerExceptionMessage, content: content);
+        }
+
+        public Response ServiceUnavailable(string message, string? innerExceptionMessage = null, object? content = null)
+        {
+            return new Response(HttpStatusCode.ServiceUnavailable, exceptionMessage: message, innerExceptionMessage: innerExceptionMessage, content: content);
+        }
+
+        public Response BadGateway(string message, string? innerExceptionMessage = null, object? content = null)
+        {
+            return new Response(HttpStatusCode.BadGateway, exceptionMessage: message, innerExceptionMessage: innerExceptionMessage, content: content);
+        }
+
+        public Response GatewayTimeout(string message, string? innerExceptionMessage = null, object? content = null)
+        {
+            return new Response(HttpStatusCode.GatewayTimeout, exceptionMessage: message, innerExceptionMessage: innerExceptionMessage, content: content);
         }
     }
 }
